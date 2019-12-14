d806d3454ff683d442b0f4a68e26db2d68855299
Ming Cheng <mcheng991129@gmail.com>
2019-12-14 16:32:02 +0800

    Add files via upload

---
+++ homework5/svm.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import csv
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+# Load data
+def load_csv_data(filename):
+    file = pd.read_csv(filename)
+
+    data = file[['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology',
+                 'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points',
+                 'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3', 'Wilderness_Area4', 'Soil_Type1',
+                 'Soil_Type2', 'Soil_Type3', 'Soil_Type4', 'Soil_Type5', 'Soil_Type6',
+                 'Soil_Type7', 'Soil_Type8', 'Soil_Type9', 'Soil_Type10', 'Soil_Type11',
+                 'Soil_Type12', 'Soil_Type13', 'Soil_Type14', 'Soil_Type15', 'Soil_Type16',
+                 'Soil_Type17', 'Soil_Type18', 'Soil_Type19', 'Soil_Type20', 'Soil_Type21',
+                 'Soil_Type22', 'Soil_Type23', 'Soil_Type24', 'Soil_Type25', 'Soil_Type26',
+                 'Soil_Type27', 'Soil_Type28', 'Soil_Type29', 'Soil_Type30', 'Soil_Type31',
+                 'Soil_Type32', 'Soil_Type33', 'Soil_Type34', 'Soil_Type35', 'Soil_Type36',
+                 'Soil_Type37', 'Soil_Type38', 'Soil_Type39', 'Soil_Type40']]
+    labels = file['Cover_Type']
+    data = np.array(data)
+    labels = np.array(labels)
+    return data, labels
+
+# Load data
+def load_csv_test(filename):
+    file = pd.read_csv(filename)
+
+    data = file[['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology',
+                 'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points',
+                 'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3', 'Wilderness_Area4', 'Soil_Type1',
+                 'Soil_Type2', 'Soil_Type3', 'Soil_Type4', 'Soil_Type5', 'Soil_Type6',
+                 'Soil_Type7', 'Soil_Type8', 'Soil_Type9', 'Soil_Type10', 'Soil_Type11',
+                 'Soil_Type12', 'Soil_Type13', 'Soil_Type14', 'Soil_Type15', 'Soil_Type16',
+                 'Soil_Type17', 'Soil_Type18', 'Soil_Type19', 'Soil_Type20', 'Soil_Type21',
+                 'Soil_Type22', 'Soil_Type23', 'Soil_Type24', 'Soil_Type25', 'Soil_Type26',
+                 'Soil_Type27', 'Soil_Type28', 'Soil_Type29', 'Soil_Type30', 'Soil_Type31',
+                 'Soil_Type32', 'Soil_Type33', 'Soil_Type34', 'Soil_Type35', 'Soil_Type36',
+                 'Soil_Type37', 'Soil_Type38', 'Soil_Type39', 'Soil_Type40']]
+    data = np.array(data)
+    return data
+
+
+def svm(kernel, features, labels, features_test, labels_test):
+
+    rf2 = SVC(kernel = kernel)
+
+    ## train
+    rf2.fit(features, labels)
+
+    ## predict
+    predict = rf2.predict(features_test)
+
+    print(kernel, '\t', accuracy_score(labels_test, predict))
+
+    # write csv
+    with open("all/predict_svm_" + kernel + ".csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(
+            ["Id", "Cover_Type"])
+        for i in range(0, len(predict)):
+            writer.writerow([(15121 + i), predict[i]])
+
+
+
+if __name__ == '__main__':
+    features, labels = load_csv_data('all/train.csv')
+
+    features_test = load_csv_test('all/test.csv')
+
+    print('svm\r')
+
+    kf = KFold(n_splits = 5)
+
+    kernel_lis = ['rbf', 'sigmoid', 'linear', 'poly']
+
+    # for kernel in kernel_lis:
+
+    for train_index, test_index in kf.split(features):
+        train_X, train_y = features[train_index], labels[train_index]
+        test_X, test_y = features[test_index], labels[test_index]
+
+
+    ## Here we take 'rbf' as example...
+        svm('rbf', train_X, train_y, test_X, test_y)
+
+        
